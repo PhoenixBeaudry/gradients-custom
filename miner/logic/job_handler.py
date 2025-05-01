a5b320b1de7f376b0c7098da08c6bd37fdc825f4
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-04-30 18:03:59 -0700

    hp changes for oom

--- miner/logic/job_handler.py
+++ miner/logic/job_handler.py
@@ -106,7 +106,6 @@ def _load_and_modify_config(
         if config["model_params_count"] < 4_000_000_000:
             # If less than 4b do full finetune with higher LR
             config["learning_rate"] = 4e-4
-            config["adapter"] = None
             pass
 
     config["mlflow_experiment_name"] = dataset
